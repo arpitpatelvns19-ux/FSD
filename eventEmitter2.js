da6291468816f6cdf4a99229aaa8adc508cbd8f9
arpitpatelvns19-ux <arpitpatelvns19@gmail.com>
2026-07-21 10:05:44 +0530

    new

--- eventEmitter2.js
+++ eventEmitter2.js
@@ -1,11 +1,26 @@
 import {EventEmitter} from 'node:events'
 
-const sayHi =(name)=>{
-    console.log(`${name} logged in`);
+const sayHi = (name) => {
+    console.log(`${name} logged in `);
+    
 };
 
 const task = new EventEmitter();
 
-task.on ("greet",sayHi);
-
+task.on('greet',sayHi);
+task.on("greet",(name)=> {
+    console.log(`${name} starts working`);
+    
+})
+task.on("exit",(name)=>{
+    console.log("System is Shutting Down...");
+    
+});
 task.emit("greet","Rahul Singh");
+task.emit("greet","Aryan Choudhary");
+task.emit("exit");
+
+task.off("greet",sayHi);
+task.emit("greet","Manager");
+task.emit("greet","Employee");
+console.log(task.listenerCount);

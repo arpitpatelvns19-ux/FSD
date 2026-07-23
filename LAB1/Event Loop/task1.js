b4494ae52520c747337cf62dd68bc05155e16acf
arpitpatelvns19-ux <arpitpatelvns19@gmail.com>
2026-07-23 15:28:24 +0530

    go

--- LAB1/Event Loop/task1.js
+++ LAB1/Event Loop/task1.js
@@ -1,17 +1,24 @@
-const f1 =()=> {
-    console.log("f1");
+const f1 = () => {
+  console.log("f1 starts");
+  f2();
+  console.log("f1 running");
+  console.log("f1 end");
 };
-const f2 =()=> {
-    console.log("f2");
+const f2 = () => {
+  console.log("f2 starts");
+  f3();
+  console.log("f2 running");
+  console.log("f2 ends");
 };
-const f3 =()=> {
-    console.log("f3");
+const f3 = () => {
+  console.log("f3 starts");
+  console.log("f3 running");
+  console.log("f3 end");
 };
-function main (){
-    console.log("main");
-    f1();
-    f2();
-    f3();
-    console.log("end main");
+function main() {
+  console.log("main");
+  f1();
+  console.log("end main");
 }
 main();
+//javascript is syncronous and single threaded

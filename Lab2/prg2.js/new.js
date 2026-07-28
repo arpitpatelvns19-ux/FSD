d89abaeaf7663e9c695a08474478b0eeeb7d7557
arpitpatelvns19-ux <arpitpatelvns19@gmail.com>
2026-07-28 10:29:39 +0530

    g

---
+++ Lab2/prg2.js/new.js
@@ -0,0 +1,17 @@
+import { mkdir, rm } from "fs/promises";
+
+//await mkdir("uploads");
+
+//await mkdir("uploads/images");
+
+//creates main and sub folder
+//await mkdir("docs/resumes/data", { recursive: true});
+
+//removes only data folder
+//await rm("docs/resumes/data", {recursive: true});
+
+//removes main folder and sub folder also
+//await rm("docs", { recursive: true});
+
+// remove main folder and sub folder also
+await rm("docs",{recursive:true});
